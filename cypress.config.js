28c29d2daf5d0f060106bc12f599fc7317cdf684
Mashruf Rohon <mashrufrohon@gmail.com>
2025-06-03 01:59:27 +0600

    added CI file

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,22 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'report/html',
+    overwrite: true,
+    html: true,
+    json: true,
+    embeddedScreenshots: true,
+    inlineAssets: true
+  },
+  video: false,
+  screenshotOnRunFailure: true,
   e2e: {
     baseUrl: 'https://www.saucedemo.com/',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
