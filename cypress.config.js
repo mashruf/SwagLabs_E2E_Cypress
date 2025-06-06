b84ad65e52a9b17e3003d23bad970e3589c66527
Mashruf Rohon <mashrufrohon@gmail.com>
2025-06-06 20:29:59 +0600

    allure report and yml config updated

--- cypress.config.js
+++ cypress.config.js
@@ -2,16 +2,16 @@ const { defineConfig } = require('cypress');
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
-video: false,
-screenshotOnRunFailure: true,
-e2e: {
-baseUrl: 'https://www.saucedemo.com/',
-setupNodeEvents(on, config) {
-allureWriter(on, config);
-return config;
-},
-},
-env: {
-allure: true,
-}
+  video: false,
+  screenshotOnRunFailure: true,
+  e2e: {
+    baseUrl: 'https://www.saucedemo.com/',
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    },
+  },
+  env: {
+    allure: true,
+  },
 });
